02a331a81bc2a976805f2f4314f1ccc4c3b1a3b8
Vaibhav Parmar <vaibhav@nickelfox.com>
2019-01-31 11:00:50 +0530

    feat(project): add project files

---
+++ FoxCast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoxCast
+//
+//  Created by Vaibhav Parmar on 31/01/19.
+//  Copyright © 2019 Nickelfox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
