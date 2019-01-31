fb539f681f0e3ca4d1c86a30d4464e004d8d3cdb
Vaibhav Parmar <vaibhav@nickelfox.com>
2019-01-31 15:06:24 +0530

    feat(onboarding): add background and login button

--- FoxCast/ViewController.swift
+++ FoxCast/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet var loginButton: UIButton!
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
